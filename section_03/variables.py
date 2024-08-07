0a9c06b192c872cc46a4d55d8809ffd273fa2652
Jesus Marquez <jesus.marquez.dev@gmail.com>
2024-08-07 10:53:45 -0400

    Created script with example of variable initialization

---
+++ section_03/variables.py
@@ -0,0 +1,14 @@
+# Example of values declaration and assignation
+# name = "Maria"
+# age = 30
+# weight = 65.5
+# is_married = False
+
+age = 28
+height = 1.65
+country = 'Mexico'
+
+# Access to variables
+print('Age:', age)
+print('Height:', height)
+print('Country:', country)
