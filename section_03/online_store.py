ef4b34c5a3544bc5d1cde51bed4eda0972a9c4ee
Jesus Marquez <jesus.marquez.dev@gmail.com>
2024-08-08 18:14:32 -0400

    Online Store Example

---
+++ section_03/online_store.py
@@ -0,0 +1,34 @@
+# Booking welcome message
+WELCOME_MESSAGE = '*** ONLINE STORE SYSTEM ***'
+
+# First booking example
+product = 'Cámara Digital'
+price = 399.99
+inventory_stock = 20
+is_available = True
+
+print(WELCOME_MESSAGE)
+print('Product:', product)
+print('Price: $', price)
+print('Inventory Stock:', inventory_stock)
+print('Is The Product Available?:', is_available)
+
+# Second booking example
+price = 299.99
+
+print()
+print('Product:', product)
+print('Price: $', price)
+print('Inventory Stock:', inventory_stock)
+print('Is The Product Available?:', is_available)
+
+# Third booking example
+price = 449.99
+inventory_stock = 0
+is_available = False
+
+print()
+print('Product:', product)
+print('Price: $', price)
+print('Inventory Stock:', inventory_stock)
+print('Is The Product Available?:', is_available)
