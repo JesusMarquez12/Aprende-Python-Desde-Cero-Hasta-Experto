2cb3cec4a3e801a5fe59b1e9c76db40014e45f20
JESUS MARQUEZ <jesus.marquez.dev@gmail.com>
2025-03-31 17:34:40 -0300

    Example of replace method

---
+++ section_04/substring_replace.py
@@ -0,0 +1,12 @@
+# String method replace
+
+string = "Hello, World!"
+print(f'Original string: {string}')
+
+# Example of replace method
+new_string = string.replace('World', 'to all.')
+print(f'New replaced string: {new_string}')
+
+# Replace hello text with bye
+new_string = string.replace('Hello', 'Bye')
+print(f'New replaced string: {new_string}')
